81b87f7aa4492f1187ebfd4ca3c0146abb42a291
omjewellersjdp <admin@omjewellersjdp.com>
2025-07-06 15:09:44 +0530

    Delete frontend directory

--- frontend/src/utils/appStore.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import useReducer  from "./userSlice"
-
-const appStore = configureStore({
-    reducer:{
-        user:useReducer
-    }
-})
-export default appStore
